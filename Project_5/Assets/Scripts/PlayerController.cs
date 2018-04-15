7363a33851905ba73c744f8a73e5e06d13a9a1a8
ajensen11 <ajensen11@gmail.com>
2018-04-14 18:19:01 -0700

    add movement to player prefab, with network awareness

---
+++ Project_5/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        float horizontalMovement = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float verticalMovement = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        
+        // if has both movements, means player is Strafing
+        if (horizontalMovement != 0.0f && verticalMovement != 0.0f)
+        {
+            // horizontal movement equals rotation
+            float horizontalRotation = horizontalMovement * 150.0f;
+            transform.Rotate(0, horizontalRotation, 0);
+            transform.Translate(0, 0, verticalMovement);
+        }
+        else
+        {
+            // forward/back or left/right
+            float newLeftRightMovement = horizontalMovement * 3.0f;  // want left/right speed to be equal to front/back
+            transform.Translate(newLeftRightMovement, 0, verticalMovement);
+
+        }
+    }
+}
