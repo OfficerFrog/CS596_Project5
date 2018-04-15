c105eaf19d8a0b73ceca459709c45c3140281b2d
ajensen11 <ajensen11@gmail.com>
2018-04-14 18:24:29 -0700

    multiple player objects are now kept in sync; a user views their player as blue

--- Project_5/Assets/Scripts/PlayerController.cs
+++ Project_5/Assets/Scripts/PlayerController.cs
@@ -29,4 +29,9 @@ public class PlayerController : NetworkBehaviour
 
         }
     }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
 }
