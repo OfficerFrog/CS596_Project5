9cf63e051036aedd6f5fb253b33d0dfb6bccec05
ajensen11 <ajensen11@gmail.com>
2018-04-14 19:07:20 -0700

    added "gun" and ability to shoot bullets

--- Project_5/Assets/Scripts/PlayerController.cs
+++ Project_5/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,27 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
+    public GameObject ProjectilePrefab;
+    public Transform ProjectileSpawn;
+
     void Update()
     {
         if (!isLocalPlayer)
             return;
 
+        UpdateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Fire();
+    }
+
+    private void UpdateMovement()
+    {
         float horizontalMovement = Input.GetAxis("Horizontal") * Time.deltaTime;
         float verticalMovement = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-        
-        // if has both movements, means player is Strafing
+
+        // if has both movements, means player is turning in circle
         if (horizontalMovement != 0.0f && verticalMovement != 0.0f)
         {
             // horizontal movement equals rotation
@@ -28,6 +39,21 @@ public class PlayerController : NetworkBehaviour
             transform.Translate(newLeftRightMovement, 0, verticalMovement);
 
         }
+    }
+
+    void Fire()
+    {
+        // create an instane of the projectile we want to fire
+        GameObject projectile = Instantiate(
+            ProjectilePrefab,
+            ProjectileSpawn.position,
+            ProjectileSpawn.rotation);
+
+        // give the projectile some velocity
+        projectile.GetComponent<Rigidbody>().velocity = projectile.transform.forward * 7;
+
+        // destroy it after some arbitrary amount of time; 2 seconds seems good enough
+        Destroy(projectile, 2.0f);
     }
 
     public override void OnStartLocalPlayer()
