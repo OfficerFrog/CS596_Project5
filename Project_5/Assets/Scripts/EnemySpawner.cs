e84a8743b5cfd544a422e634da703ce0e9f944d2
ajensen11 <ajensen11@gmail.com>
2018-04-15 18:10:22 -0700

    added Enemy prefab; added spawning of enemies

---
+++ Project_5/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnemySpawner : NetworkBehaviour
+{
+
+    public GameObject EnemyPrefab;
+    public int NumberOfEnemies;
+
+    /// <summary>
+    /// when server starts create and spawn enemies in random spots
+    /// </summary>
+    public override void OnStartServer()
+    {
+        for (int i = 0; i < NumberOfEnemies; i++)
+        {
+            var spawnPosition = new Vector3(
+                Random.Range(-8.0f, 8.0f),
+                0.0f,
+                Random.Range(-8.0f, 8.0f));
+
+            var spawnRotation = Quaternion.Euler(
+                0.0f,
+                Random.Range(0, 180),
+                0.0f);
+
+            GameObject enemy = Instantiate(EnemyPrefab, spawnPosition, spawnRotation);
+            NetworkServer.Spawn(enemy);
+        }
+    }
+}
