1408bb4a3ed91493d0453e5fdd3962f4629e74da
vanDyk <vandyk395@hotmail.com>
2018-04-17 11:38:55 -0700

    Menu System Created
    Created bare bones setup for menu system. Buttons are active. No art added.

---
+++ Project_5/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void NextScene (string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
