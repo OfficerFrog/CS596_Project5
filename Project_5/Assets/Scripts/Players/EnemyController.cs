0b507bc01bc81c543a0ee96ee3c60c681bb8b2ea
vanDyk <vandyk395@hotmail.com>
2018-05-03 00:14:32 -0700

    Initial Enemy AI
    Starting point for enemy AI tracking and movement.

--- Project_5/Assets/Scripts/Players/EnemyController.cs
+++ Project_5/Assets/Scripts/Players/EnemyController.cs
@@ -1,9 +1,24 @@
 ﻿
 
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : BasicPlayerController
 {
+    Transform playerPos;
+    Health playerHealth;
+    NavMeshAgent nav;
+
+    private void Awake()
+    {
+        playerPos = GetNearestPlayer();
+    }
+
+    private void Update()
+    {
+        FollowPlayer(playerPos);
+    }
+
     // TODO: make experiene gained per enemy configurable (e.g. stronger enemies give more experience)
     public override ObjectWithExperience ExperienceData
     {
@@ -24,5 +39,39 @@ public class EnemyController : BasicPlayerController
     public override void EnemyKilled(DismissibleObjectController enemy)
     {
         throw new System.NotImplementedException();
+    }
+
+    // returns location of nearest player
+    public Transform GetNearestPlayer()
+    {
+        GameObject[] player;
+        GameObject nearest = null;
+        player = GameObject.FindGameObjectsWithTag("Player");
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;  
+        foreach (GameObject go in player)
+        {
+            Vector3 diff = go.transform.position - position;
+            float currDistance = diff.sqrMagnitude;
+            if (currDistance < distance)
+            {
+                nearest = go;
+                distance = currDistance;
+            }
+        }
+        playerHealth = nearest.GetComponent<Health>();
+        return nearest.transform;
+    }
+
+    public void FollowPlayer(Transform playerTrack)
+    {
+        if (playerHealth.CurrentHealth < 1){
+
+            nav.SetDestination(playerTrack.position);
+        }
+        else
+        {
+            nav.enabled = false;
+        }
     }
 }
