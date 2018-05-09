f3e5900211fa943e1496b391febc6894ef90eba1
vanDyk <vandyk395@hotmail.com>
2018-05-08 21:45:14 -0700

    Revert "Revert "Revert "Adding files missed in last commit"""
    This reverts commit aa67a0097ab41d1fc1f16bcd369deaecc636f27a.

--- Project_5/Assets/Scripts/Players/EnemyController.cs
+++ Project_5/Assets/Scripts/Players/EnemyController.cs
@@ -22,6 +22,8 @@ public class EnemyController : BasicPlayerController
     [HideInInspector]
     private bool _isMovingRandomly;
        
+    Projectile projectile;
+
     /// <summary>
     /// point on navMesh that the enemy is currently moving towards
     /// </summary>
@@ -50,7 +52,7 @@ public class EnemyController : BasicPlayerController
         if (_engagedPlayer != null)
         {
             if (CanFire())
-                _projectile.CmdFire(_projectileSpawn.position, _projectileSpawn.forward);
+                projectile.CmdFire(_projectileSpawn.position, _projectileSpawn.forward);
         }
     }
 
